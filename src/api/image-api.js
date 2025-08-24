28b36ac118a3d2b01bc6dbefd60d5a7381bb4b69
görkem can aldı <gorkem.aldi2003@gmail.com>
2025-08-24 08:48:24 +0300

    end

--- src/api/image-api.js
+++ src/api/image-api.js
@@ -1,18 +1,24 @@
 import axios from "axios";
 
+const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_KEY;
 const BASE_URL = "https://api.unsplash.com/search/photos";
 
 export const fetchImages = async (query, page = 1) => {
-  const response = await axios.get(BASE_URL, {
-    params: {
-      query,
-      page,
-      per_page: 12,
-    },
-    headers: {
-      Authorization: `Client-ID ${import.meta.env.VITE_ACCESS_KEY}`,
-    },
-  });
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        query,
+        page,
+        per_page: 12,
+      },
+      headers: {
+        Authorization: `Client-ID ${ACCESS_KEY}`,
+      },
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching images:", error);
+    throw error;
+  }
 };
